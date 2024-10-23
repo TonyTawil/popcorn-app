52ace22a1ef6074d17af4a9ed0effe4691de8c46
VeronicaHoyek <202011458@ua.edu.lb>
2024-10-23 13:48:00 +0200

    added the get-watched endpoint

--- backend/backend/controllers/movie.controller.js
+++ backend/backend/controllers/movie.controller.js
@@ -138,3 +138,20 @@ export const getWatchlist = async (req, res) => {
       .send({ message: "Error fetching watchlist", error: error.message });
   }
 };
+
+export const getWatched = async (req, res) => {
+  const { userId } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.status(200).send({ watched: user.watched });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Error fetching watched list", error: error.message });
+  }
+};
