a754716c56b6a8b4388dee31a2eb3ad71a537e9d
VeronicaHoyek <202011458@ua.edu.lb>
2024-10-23 16:09:22 +0200

    is-verified endpoint

--- backend/backend/controllers/auth.controller.js
+++ backend/backend/controllers/auth.controller.js
@@ -143,3 +143,21 @@ export const verifyEmail = async (req, res) => {
       .json({ error: "An error occurred during email verification" });
   }
 };
+
+export const isEmailVerified = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ isEmailVerified: user.isEmailVerified });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "An error occurred while checking email verification status",
+    });
+  }
+};
