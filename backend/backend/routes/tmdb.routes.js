10370a32e7e2fb4936981c7d7648234c5f7e0597
VeronicaHoyek <202011458@ua.edu.lb>
2024-11-01 12:13:07 +0100

    removed the old endpoints

--- backend/backend/routes/tmdb.routes.js
+++ backend/backend/routes/tmdb.routes.js
@@ -1,11 +1,3 @@
-// https://api.themoviedb.org/3/trending/movie/day?api_key=[API_KEY]&page=[PAGE_NUMBER]
-// https://api.themoviedb.org/3/movie/[MOVIE_TYPE]?api_key=[API_KEY]&page=[PAGE_NUMBER]
-// https://api.themoviedb.org/3/movie/[MOVIE_ID]/credits?api_key=[API_KEY]
-// https://api.themoviedb.org/3/trending/movie/day?api_key=key&page=number
-// https://api.themoviedb.org/3/movie/now_playing?api_key=key&page=number
-// https://api.themoviedb.org/3/movie/upcoming?api_key=key&page=number
-// https://api.themoviedb.org/3/movie/{MOVIE_ID}/credits?api_key=key
-
 import express from "express";
 import {
   getTrending,
